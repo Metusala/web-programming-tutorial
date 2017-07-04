e5f72f537cc5d140b239e119be0dca6a85360c95
Cristian <suteu63@gmail.com>
2017-07-04 19:56:54 +0300

    convert from array of arrats into json

--- js/contacte.js
+++ js/contacte.js
@@ -1,24 +1,51 @@
 
 
 function getRow(firsName, lastName, phone) {
-    var row = '<tr> <td>' + lastName + '</td><td>' + firsName + '</td><td>'+ phone +'</td></tr>';
+    //se poate asa!!
+    if(phone == undefined){
+        phone = '';
+    };
+    // sau asas!!
+    if(typeof lastName == 'undefined'){
+        lastName = '';
+    };
+    //sau asa cu linute sau fara!!
+    // firsName = firsName || '---';
+    firsName = firsName || '---';
+    var row = '<tr> <td>' + lastName + '</td><td>' + (firsName || '') +  '</td><td>'+ phone +'</td></tr>';
     return row;
 
 
 }
+var gerRow = function( firsName, lastName, phone ){
+    var row = '<tr> <td>' + lastName + '</td><td>' + firsName + '</td><td>'+ phone +'</td></tr>';
+    return row;
+    // identic ca susu, pot fi scrise la fel ca sus sau jos!!!!
 
+};
+
+// matrice
+// Array of arrays
 var contacte = [
-        ['Nicolae', 'M', '01'],
-        ['Cristian','S', '02'],
-        ['Silviu',  'R',  '03'],
-        ['Ionut',   'A',   '04']
+    {firstName:'Nicolae', lastName: 'M', phone: '01' },
+    {firstName:'Cristian', lastName: 'S', phone:'02'  },
+    {firstName:'Silviu', lastName:  'R', phone:  '03'},
+    {firstName: 'Ionut', lastName:  'A',  phone: '04'},
+    {firstName: 'Andrei'  },
+    {lastName: 'Basescu'}
 
 
 ];
+
+
 var tableContent = '';
 for (var i = 0; i< contacte.length; i++){
 var contact =contacte [i];
-    tableContent += getRow (contact[0], contact[1], contact[2]);
+    tableContent += getRow (contact.firstName, contact.lastName, contact.phone);
 }
 $("#contacts-list tbody").html(tableContent);
 
+// 1.convert from array of arrats into json
+// 2. load contacts from json file with AJAX
+// 3. remove contacts (UI)
+// 4. EDIT CONTACT (UI)
