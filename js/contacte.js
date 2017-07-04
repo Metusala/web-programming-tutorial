413585c44bcb78ea070173edfe75d301517f1a92
Cristian <suteu63@gmail.com>
2017-07-04 20:25:56 +0300

    forEach

--- js/contacte.js
+++ js/contacte.js
@@ -4,11 +4,11 @@ function getRow(firsName, lastName, phone) {
     //se poate asa!!
     if(phone == undefined){
         phone = '';
-    };
+    }
     // sau asas!!
     if(typeof lastName == 'undefined'){
         lastName = '';
-    };
+    }
     //sau asa cu linute sau fara!!
     // firsName = firsName || '---';
     firsName = firsName || '---';
@@ -17,7 +17,7 @@ function getRow(firsName, lastName, phone) {
 
 
 }
-var gerRow = function( firsName, lastName, phone ){
+var gerRow2 = function( firsName, lastName, phone ){
     var row = '<tr> <td>' + lastName + '</td><td>' + firsName + '</td><td>'+ phone +'</td></tr>';
     return row;
     // identic ca susu, pot fi scrise la fel ca sus sau jos!!!!
@@ -33,17 +33,25 @@ var contacte = [
     {firstName: 'Ionut', lastName:  'A',  phone: '04'},
     {firstName: 'Andrei'  },
     {lastName: 'Basescu'}
-
-
-];
-
+    ];
 
 var tableContent = '';
-for (var i = 0; i< contacte.length; i++){
-var contact =contacte [i];
-    tableContent += getRow (contact.firstName, contact.lastName, contact.phone);
+
+function createRow (contact) {
+    tableContent += getRow(contact.firstName, contact.lastName, contact.phone);
 }
+// for (var i = 0; i< contacte.length; i++){
+//     createRow(contacte[i]);
+
+
+contacte.forEach(createRow);
+
+
+
 $("#contacts-list tbody").html(tableContent);
+
+
+
 
 // 1.convert from array of arrats into json
 // 2. load contacts from json file with AJAX
